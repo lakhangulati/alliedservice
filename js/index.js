9ff382e328c5566ecf78ac00bb762894c4a7a142
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2014-09-08 13:11:43 +0530

    modified to redesign the interface

--- js/index.js
+++ js/index.js
@@ -1,24 +1,62 @@
-var _updcounter  = 0;
+var modSessions = {};
+modSessions.setUserName = function (username) {
+	modSessions._LoggedInUser = username;
+};
+
+modSessions.setLineNo = function (lineNo) {
+	modSessions._line = lineNo;
+};
+
+modSessions.getLineNo = function () {
+	if (typeof modSessions._line === 'undefined') {
+		modSessions._line = -1 ;
+	};
+	return modSessions._line;
+};
+
+$(document).ajaxError(function(xhr, error) {
+	 alert( "Triggered ajaxError handler." );
+});
+
+
+__showLineDetails = function (data, textStatus, jqXHR) {
+//	var HeroUnits = '<h1><span class="label label-primary">Active Sessions</span></h1>';
+	var HeroUnits = '<div class="jumbotron">';
+	var line = modSessions.getLineNo();
+
+		$.get( "modules/sessions/moduleEntry.php", {action:'getLineDetails',line:line}, function( data ) {
+		if ( data.callstatus == "OK" ) {
+			if ( data.found > 0 ) {
+				HeroUnits = HeroUnits + "<h3>" + data.fullname + "</h3>";
+				HeroUnits = HeroUnits + "<h4>" + data.sector + "</h4>";
+				HeroUnits = HeroUnits + "<h5>" + data.contact + "</h5>";
+				HeroUnits = HeroUnits + "<h5>" + data.address + "</h5>";
+				HeroUnits = HeroUnits + "</div>" ;
+				//alert(JSON.stringify(data));
+				$("#linedetails").html(HeroUnits);
+			} else {
+				HeroUnits = HeroUnits + "<h3> There is no line with number " + data.line + "</h3>";
+				HeroUnits = HeroUnits + "</div>" ;
+				$("#linedetails").html(HeroUnits);
+			}
+		} 
+	});
+};
 
 
 __showActiveSessions = function (data, textStatus, jqXHR) {
 
-	var SelectOptions = "";
-	SelectOptions = SelectOptions + "<table class=\"table\"><thead><tr><th>Doctor</th><th>Clinic Location</th><th>Patient Number</th><th>Service Rate</th><th>Last Updated</th></tr></thead><tbody>";
-
-	var HeroUnits = '<h1><span class="label label-primary">Active Sessions</span></h1>';
-	$.post( "modules/sessions/moduleEntry.php", {action:'getActiveSessions'}, function( data ) {
+	var HeroUnits = '<div class="jumbotron">';
+	var line = modSessions.getLineNo();
+	
+		$.get( "modules/sessions/moduleEntry.php", {action:'getSessionsById',line:line}, function( data ) {
 		if ( data.callstatus == "OK" ) {
 			var sessions = data.sessions;
-			
 
 			for (var i = 0; i < sessions.length; i++) {
 				var sobj = sessions[i];
 				
-				var fullname = sobj.fullname;
-				var sname = sobj.sname;
 				var counter = sobj.counter;
-
 				var started = sobj.started;
 				var updated = sobj.updated;
 				var tmnow = sobj.tmnow;
@@ -41,22 +79,21 @@ __showActiveSessions = function (data, textStatus, jqXHR) {
 
 				lastupdated = Math.round(lastupdated) ;
 				
-				var itmsession = sname ;
 				var itmcounter = "<span class=\"label label-success\">" + counter + "</span>";
 				var itmrate = "<span class=\"label label-info\">" + servicerate + " minutes / 10 patients</span>";
+
+				if ( counter <= 0 ) {
+					itmcounter = "<span class=\"label label-danger\">" + counter + "</span>";
+					itmrate = '<span class="label label-default"> Not Started Yet</span>';
+				} 
+				
 				var itmlastupd = "<span class=\"label label-info\">" + lastupdated + lastupdateunit + " before</span>";
 				
-				//SelectOptions = SelectOptions + "<p>" + fullname + "|" + sname + "|" + counter + "</p>";
-				HeroUnits = HeroUnits + "<div class=\"col-md-3 hero-unit\">";
-				HeroUnits = HeroUnits + "<h1>" + fullname + "</h1>";
-				HeroUnits = HeroUnits + "<h2>" + itmsession + "</h2>";
 				HeroUnits = HeroUnits + "<h3>" + itmcounter + "</h3>";
 				HeroUnits = HeroUnits + "<h4>" + itmrate + "</h4>";
 				HeroUnits = HeroUnits + "<h5>" + itmlastupd + "</h5>";
 				HeroUnits = HeroUnits + "</div>";
-
 			}
-			SelectOptions = SelectOptions + "</tbody></table>";
 
 			//alert(JSON.stringify(data));
 			$("#activesessionshero").html(HeroUnits);
@@ -68,13 +105,17 @@ __showActiveSessions = function (data, textStatus, jqXHR) {
 	});
 };
 
-
 setInterval(function() {
-	_updcounter = _updcounter + 1;
-	$('#vasCounter').text(_updcounter);
-	$('#gzbCounter').text(_updcounter);
-
 	__showActiveSessions();
+	__showLineDetails();
 	
 	
-}, 5000);
+}, 10000);
+
+$(document).ready(function (e) { // pass the event object
+    $("#submitLineScuscribe").click(function(){
+    	modSessions.setLineNo ( $('#lineToSubscribe').val());
+    	__showActiveSessions();
+    	__showLineDetails();
+    });
+});
