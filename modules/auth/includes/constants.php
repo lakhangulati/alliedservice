18768040eec6cab40a9b0b2dee226e0866af3a7f
lakhangulati <lakhangulati@users.noreply.github.com>
2015-04-27 20:51:36 +0530

    Update constants.php

--- modules/auth/includes/constants.php
+++ modules/auth/includes/constants.php
@@ -3,6 +3,6 @@
 // Define constants here
 
 define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'pwd');
+define('DB_USER', 'krish933_qdev');
+define('DB_PASSWORD', 'Ub@2ntu87');
 define('DB_NAME', 'qm');
